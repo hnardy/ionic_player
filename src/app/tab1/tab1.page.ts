b9e83976550ce0680eb15c2cccf0b9087d82aa55
Henrique_Nardy <hnardyluz@gmail.com>
2025-04-06 23:33:10 -0300

    Revert "teste para online"
    This reverts commit 894e4129dfc393acb2600ac0a48944453bda6fd4.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,116 +1,20 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-
-interface Song {
-  title: string;
-  artist: string;
-  url: string;
-}
 
 @Component({
   selector: 'app-tab1',
-  standalone: true,
-  imports: [IonicModule],
+  standalone: true, // 👈 importante no Angular standalone
+  imports: [IonicModule], // 👈 registra os componentes Ionic aqui
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements AfterViewInit {
+export class Tab1Page {
   @ViewChild('audioPlayer', { static: false }) audioPlayer!: ElementRef<HTMLAudioElement>;
-
-  songs: Song[] = [
-    {
-      title: 'Dancing In The Street',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/01%20-%20Dancing%20In%20The%20Street.mp3',
-    },
-    {
-      title: "I Can't Wait",
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/02%20-%20I%20Can%27t%20Wait.mp3',
-    },
-    {
-      title: 'Monday Monday',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/03%20-%20Monday%20Monday.mp3',
-    },
-    {
-      title: 'Straight Shooter',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/04%20-%20Straight%20Shooter.mp3',
-    },
-    {
-      title: 'I Call Your Name',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/05%20-%20I%20Call%20Your%20Name.mp3',
-    },
-    {
-      title: 'California Dreamin',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/06%20-%20California%20Dreamin%27.mp3',
-    },
-    {
-      title: 'Somebody Groovy',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/07%20-%20Somebody%20Groovy.mp3',
-    },
-    {
-      title: 'In Crowd',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/08%20-%20In%20Crowd.mp3',
-    },
-    {
-      title: 'My Girl',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/09%20-%20My%20Girl.mp3',
-    },
-    {
-      title: 'Creque Alley',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/10%20-%20Creque%20Alley.mp3',
-    },
-    {
-      title: 'Free Advice',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/11%20-%20Free%20Advice.mp3',
-    },
-    {
-      title: 'Boys & Girls Together',
-      artist: 'The Mamas and Papas',
-      url: 'https://dn721907.ca.archive.org/0/items/mamaspapas/12%20-%20Boys%20%26%20Girls%20Together.mp3',
-    },
-    {
-      title: 'Gemini Child',
-      artist: 'The Mamas and Papas',
-      url: 'https://ia801806.us.archive.org/10/items/mamaspapas/13%20-%20Gemini%20child.mp3',
-    },
-
-    // Exemplo de outra banda
-    {
-      title: 'Imagine',
-      artist: 'John Lennon',
-      url: 'https://archive.org/download/Imagine_201303/01%20Imagine.mp3',
-    },
-  ];
-
-  currentIndex = 0;
-  currentSong: Song = this.songs[this.currentIndex];
   isPlaying = false;
 
-  ngAfterViewInit() {
-    this.setAudioSource();
-  }
-
-  setAudioSource() {
-    const audio: HTMLAudioElement = this.audioPlayer.nativeElement;
-    audio.src = this.currentSong.url;
-    audio.load();
-    if (this.isPlaying) {
-      audio.play();
-    }
-  }
-
   togglePlay() {
-    const audio: HTMLAudioElement = this.audioPlayer.nativeElement;
+    const audio = this.audioPlayer.nativeElement;
+
     if (this.isPlaying) {
       audio.pause();
     } else {
