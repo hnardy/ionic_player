894e4129dfc393acb2600ac0a48944453bda6fd4
Henrique_Nardy <hnardyluz@gmail.com>
2025-04-06 22:18:25 -0300

    teste para online

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,25 +1,26 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicModule } from '@ionic/angular'; // 👈 importa o módulo do Ionic
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
-  standalone: true, // 👈 importante no Angular standalone
-  imports: [IonicModule], // 👈 registra os componentes Ionic aqui
+  standalone: true,
+  imports: [IonicModule],
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  isPlaying = false;
-
   @ViewChild('audioPlayer', { static: false }) audioPlayer!: ElementRef<HTMLAudioElement>;
+  isPlaying = false;
 
   togglePlay() {
     const audio = this.audioPlayer.nativeElement;
+
     if (this.isPlaying) {
       audio.pause();
     } else {
       audio.play();
     }
+
     this.isPlaying = !this.isPlaying;
   }
 }
